3462775d07e3ff021ccbe4bbcae7c7866d13c073
chriscarolan <89954045+chriscarolan@users.noreply.github.com>
2022-03-03 16:43:34 -0500

    Lab 6
    Lab 6 finish

--- JavaLabs/Lab 6/src/base/Main.java
+++ JavaLabs/Lab 6/src/base/Main.java
@@ -1,5 +1,10 @@
 package base;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class Main {
     /**
      * # Lab Six
@@ -23,10 +28,45 @@ public class Main {
      */
     public static void main(String[] args) {
         // Create an Array, Populate it, and Print it out
+        int[] Array = new int[]{1,2,3,4,5};
+        for(int i=0; i<5; i++){
+            System.out.println(Array[i]);
+        }
+        System.out.println();
         // Create an ArrayList, Populate it, and Print it out
+        ArrayList<Integer> intArray = new ArrayList<Integer>();
+        intArray.add(1);
+        intArray.add(3);
+        for(int i=0; i<intArray.size(); i++) {
+            System.out.println(intArray.get(i));
+        }
+        System.out.println();
         // Create an LinkedList, Populate it, and Print it out
-        // Create an Queue, Populate it, and Print it out
-        // Create an Stack, Populate it, and Print it out
-        // Using the information in the Lab 6 Jupyter Notebook create an instance of plant here.
+        LinkedList<Integer> intList = new LinkedList<Integer>();
+        intList.add(5);
+        intList.add(8);
+        for(int i=0; i<intList.size(); i++) {
+            System.out.println(intList.get(i));
+        }
+        System.out.println();
+        // Create a Queue, Populate it, and Print it out
+        Queue<Integer> intQueue = new LinkedList<>();
+        intQueue.add(4);
+        intQueue.add(6);
+        int initialSize = intQueue.size();
+        for(int i=0; i<initialSize; i++) {
+            System.out.println(intQueue.remove());
+        }
+        System.out.println();
+        // Create a Stack, Populate it, and Print it out
+        Stack<Integer> intStack = new Stack<Integer>();
+        intStack.add(3);
+        intStack.add(7);
+        while(!intStack.empty()){
+            System.out.println(intStack.pop());
+        }
+        // Using the information in the Lab 6 J
+        // upyter Notebook create an instance of plant here.
+        Plants plant = new Plants(true, "sunflower", "rose");
     }
 }
